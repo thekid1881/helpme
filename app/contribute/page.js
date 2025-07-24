d526a30ae57dbe119b450af16d65f744993a598b
Sarah Bill <sarahbill33@gmail.com>
2025-07-24 01:01:58 -0400

    second commit

--- app/contribute/page.js
+++ app/contribute/page.js
@@ -6,11 +6,11 @@ export default function Contribute() {
     return (
         <div>
             <div className="font-sans bg-[#94cce0] items-center justify-items-center min-h-screen pt-12">
-                <h1 className="text-center text-5xl font-bold mb-12">
+                <h1 className="text-center text-5xl font-bold mb-24">
                     Please Contribute
                 </h1>
                 <ContributeInfo />
-                <div className="flex flex-row items-center justify-self-center mt-8 mb-6">
+                <div className="flex flex-row items-center justify-self-center mt-16 mb-6">
                     <Link
                         href='/'
                         className="bg-[#3f44e0] text-white border-solid border-2 border-gray-600 rounded-md p-4 m-6"
--- components/ContributeInfo.js
+++ components/ContributeInfo.js
@@ -1,9 +1,11 @@
 export default function ContributeInfo() {
   return (
     <div className="justify-self-center text-center w-250">
-        <h2 className="bg-[#e1e2f5] ml-12 text-xl border-solid border-2 border-gray-800 rounded-md p-8">
-            Link to URL: XXX
-        </h2>
+        <a className="bg-[#e1e2f5] decoration-solid decoration-2 text-xl border-solid border-2 border-gray-800 rounded-md p-8"
+            href="https://donations.signupgenius.com/help-after-domestic-abuse"
+        >
+            Contribute Here
+        </a>
     </div>
   );
 }
